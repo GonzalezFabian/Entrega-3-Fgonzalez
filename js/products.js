a5f07a0289af93936d0704408002ffa2ab6769a5
GonzalezFabian <tenoch86@msn.com>
2023-02-28 09:30:30 -0600

    entrega3

---
+++ js/products.js
@@ -0,0 +1,34 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "CADENA",
+    precio: 200,
+    img:
+      "https://cdn.brujulabike.com/wp-content/uploads/2016/05/cambiar-la-cadena.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "FRENOS",
+    precio: 300,
+    img:
+      "https://pedalia.cc/wp-content/uploads/2020/02/Downhill_Avid.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "CUADROS",
+    precio: 1150,
+    img:
+      "https://pedalia.cc/wp-content/uploads/2021/06/carbono-bici.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "SUSPENSIÓN",
+    precio: 1561,
+    img:
+      "https://blog.trekbikes.com/wp-content/uploads/2020/07/Amortiguador_top-fuel.jpg",
+    cantidad: 1,
+  },
+];
